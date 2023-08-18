085ebcdb8ec40d6adb08922de036de642c004bbd
huzaifa003 <buntyhuzaifa3@gmail.com>
2023-08-18 20:11:32 +0500

    Completed Card

--- routes/card.js
+++ routes/card.js
@@ -12,48 +12,57 @@ router.get("/", async (req, res) => {
 })
 
 router.post("/insert", async (req, res) => {
-    let body = req.body;
-    const last_one = await Card.findOne().sort({ _id: -1 }).exec();
-    let no = 1;
-    if (last_one) {
-        console.log(last_one)
-        no = last_one.count + 1;
-    }
-    body['count'] = no;
-    body['welder_id'] = "w-" + no;
-    body['card_no'] = "c-" + no;
 
-    
-
-    let folder_name = "./uploads/" + body['card_no'];
-    console.log(folder_name)
-    fs.mkdirSync(folder_name, (err) => {
-        if (err) {
-            console.error('Error creating directory:', err);
-        } else {
-            console.log('Directory created successfully');
+    try {
+        let body = req.body;
+        const last_one = await Card.findOne().sort({ _id: -1 }).exec();
+        let no = 1;
+        if (last_one) {
+            console.log(last_one)
+            no = last_one.count + 1;
         }
-    });
+        body['count'] = no;
+        body['welder_id'] = "w-" + no;
+        body['card_no'] = "c-" + no;
 
-    if (req.files !== null) {
-        const file = req.files.card;
-        let fname = body['card_no'] + ".jpg"
-        await file.mv("./uploads/" + body['card_no'] + "/" + fname, (err) => {
-            if (err) {
-                console.error('Error moving File:', err);
-            } else {
-                console.log('File moving successfully');
-                body['image'] = body['card_no'] + ".jpg"
-            }
-        })
+
+
+        let folder_name = "./uploads/" + body['card_no'];
+        console.log(folder_name)
+        if (fs.existsSync(folder_name)) {
+            fs.mkdirSync(folder_name, (err) => {
+                if (err) {
+                    console.error('Error creating directory:', err);
+                } else {
+                    console.log('Directory created successfully');
+                }
+            });
+        }
+
+
+        if (req.files !== null) {
+            const file = req.files.card;
+            let fname = body['card_no'] + ".jpg"
+            await file.mv("./uploads/" + body['card_no'] + "/" + fname, (err) => {
+                if (err) {
+                    console.error('Error moving File:', err);
+                } else {
+                    console.log('File moving successfully');
+                    body['image'] = body['card_no'] + ".jpg"
+                }
+            })
+        }
+
+        await qrcode.toFile("./uploads/" + body['card_no'] + "/" + body['card_no'] + ".png", "http://atecosaudia-welderqualification-database.com/card/view/" + body['card_no']);
+        body['qr'] = body['card_no'] + ".png"
+        const card = await Card.create(body);
+        const record = card
+        res.render("viewCard", { 'record': record })
+        // res.send(card);
     }
-
-    await qrcode.toFile("./uploads/" + body['card_no'] + "/" + body['card_no'] + ".png", "http://atecosaudia-welderqualification-database.com/card/view/" + body['card_no']);
-    body['qr'] = body['card_no'] + ".png"
-    const card = await Card.create(body);
-    const record = card
-    res.render("viewCard", { 'record': record })
-    // res.send(card);
+    catch (err){
+        res.send(err);
+    }
 })
 
 router.get("/view/:card_no", async (req, res) => {
@@ -83,24 +92,24 @@ router.get("/edit/:card_no", async (req, res) => {
 })
 router.get("/delete/:card_no", async (req, res) => {
     try {
-      const card_no = req.params.card_no;
-      // Find the document to delete
-      const deletedRecord = await Card.findOneAndDelete({ card_no: card_no });
-  
-      if (deletedRecord) {
-        if(fs.existsSync(`./uploads/${card_no}`)){
-            fs.rmdirSync(`./uploads/${card_no}`, { recursive: true, force: true });
+        const card_no = req.params.card_no;
+        // Find the document to delete
+        const deletedRecord = await Card.findOneAndDelete({ card_no: card_no });
+
+        if (deletedRecord) {
+            if (fs.existsSync(`./uploads/${card_no}`)) {
+                fs.rmdirSync(`./uploads/${card_no}`, { recursive: true, force: true });
+            }
+            res.redirect('/supervisor')
+            // res.status(200).json({ message: "Record deleted successfully" });
+        } else {
+            res.status(404).json({ error: "Record not found" });
         }
-          res.redirect('/supervisor')
-        // res.status(200).json({ message: "Record deleted successfully" });
-      } else {
-        res.status(404).json({ error: "Record not found" });
-      }
     } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ error: "Internal server error" });
+        console.error("Error:", error);
+        res.status(500).json({ error: "Internal server error" });
     }
-  });
+});
 
 
 router.post("/update/:card_no", async (req, res) => {
@@ -153,11 +162,11 @@ router.post("/update/:card_no", async (req, res) => {
         const updatedRecord = await Card.create(body);
 
 
-        res.redirect("/card/view/"+ card_no);
+        res.redirect("/card/view/" + card_no);
     }
     catch (err) {
         console.log(err);
-        res.status(404).send("ERROR OCCURED IN UPDATIN");
+        res.status(404).send(err);
     }
 })
 module.exports = router;
